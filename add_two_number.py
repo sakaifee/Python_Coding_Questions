6af6b625ed19d644676e81bf08297731fc1eb95c
sakaifee <shadanalamkaifee@gmail.com>
2023-12-29 23:27:42 +0530

    code updated

--- add_two_number.py
+++ add_two_number.py
@@ -1,4 +1,14 @@
 num1 = 15
 num2 = 20
 sum = num1 + num2
-print("Sum of two numbers is ", sum)
+print("Sum of two numbers are ", sum)
+
+
+
+# Add two number with user input
+number1 = input("Enter the first number: ")
+number2 = input("Enter the second number: ")
+
+sum_of_number = float(number1) + float(number2)
+
+print("Sum of two numbers are: ", sum_of_number)
