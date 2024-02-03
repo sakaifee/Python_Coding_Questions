fb4e99e468c50d8bda07433a0be3d24b601ccf8c
sakaifee <shadanalamkaifee@gmail.com>
2024-02-03 23:27:35 +0530

    code updated

--- factorial_of_number.py
+++ factorial_of_number.py
@@ -3,4 +3,22 @@
 def factorial(n):
     return 1 if (n==1 or n==0) else n * factorial(n-1)
 num = 5
-print("Factorial of number is", factorial(num))
+print("Factorial of number is:", factorial(num))
+
+
+
+# Factorial of number using iterative approach
+def factorial(n):
+    if n < 0:
+        return 0
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        fact = 1 
+        while(n > 1):
+            fact *= n
+            n -= 1
+        return fact
+    
+num = 5
+print("Factorial of number is:",factorial(num))
